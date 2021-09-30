aa299ba57112a40ec6902a39864258833446e58a
tiko34 <tixonkov29@gmail.com>
2021-09-30 23:52:16 +1000

    completed 5 task android

--- app/src/main/java/com/example/fuckingbug/MainActivity.kt
+++ app/src/main/java/com/example/fuckingbug/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.fuckingbug
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-
+import android.widget.EditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
+        setContentView(R.layout.activity_main) }
     fun but(view: android.view.View) {
         var it = Intent(this@MainActivity, AboutActivity::class.java)
-        startActivity(it)
-
-
-
-
-    }
-
+        startActivity(it) }
     fun hell(view: android.view.View) {
-
         var it2 = Intent(this@MainActivity, MainActivity2::class.java)
-        startActivity(it2)
-
-    }
+        var nm1 = findViewById<EditText>(R.id.editTextTextPersonName)
+        var nm2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+        var nm11:String=nm1.text.toString()
+        var nm22:String=nm2.text.toString()
+        it2.putExtra("username", nm11)
+        it2.putExtra("gift", nm22)
+        startActivity(it2) }
 }
--- app/src/main/java/com/example/fuckingbug/MainActivity2.kt
+++ app/src/main/java/com/example/fuckingbug/MainActivity2.kt
@@ -1,40 +1,30 @@
 package com.example.fuckingbug
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.fuckingbug.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    //private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMain2Binding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.content_main)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_main2){}
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val username1 = findViewById<TextView>(R.id.textView2)
+        var user:String?=intent.extras?.getString("username")
+        var gift:String?= intent.extras?.getString("gift")
+        username1.text = "$user , вам передали $gift" }
+    private fun setContentView(activityMain2: Int, function: () -> Unit) {
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
