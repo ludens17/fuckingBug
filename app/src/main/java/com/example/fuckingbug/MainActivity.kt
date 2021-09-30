6e193df9d3d1198b1e16683b22a0eec59c3422ad
tiko34 <tixonkov29@gmail.com>
2021-10-01 00:08:55 +1000

    removed unnecessary

--- app/src/main/java/com/example/fuckingbug/MainActivity.kt
+++ app/src/main/java/com/example/fuckingbug/MainActivity.kt
@@ -14,9 +14,7 @@ class MainActivity : AppCompatActivity() {
         var it2 = Intent(this@MainActivity, MainActivity2::class.java)
         var nm1 = findViewById<EditText>(R.id.editTextTextPersonName)
         var nm2 = findViewById<EditText>(R.id.editTextTextPersonName2)
-        var nm11:String=nm1.text.toString()
-        var nm22:String=nm2.text.toString()
-        it2.putExtra("username", nm11)
-        it2.putExtra("gift", nm22)
-        startActivity(it2) }
+        it2.putExtra("username", nm1.text.toString())
+        it2.putExtra("gift", nm2.text.toString())
+        startActivity(it2)}
 }
--- app/src/main/java/com/example/fuckingbug/MainActivity2.kt
+++ app/src/main/java/com/example/fuckingbug/MainActivity2.kt
@@ -1,30 +1,13 @@
 package com.example.fuckingbug
-
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.fuckingbug.databinding.ActivityMain2Binding
-
 class MainActivity2 : AppCompatActivity() {
-    //private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setContentView(R.layout.content_main)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setContentView(R.layout.activity_main2){}
-
-
-
         val username1 = findViewById<TextView>(R.id.textView2)
         var user:String?=intent.extras?.getString("username")
         var gift:String?= intent.extras?.getString("gift")
         username1.text = "$user , вам передали $gift" }
-    private fun setContentView(activityMain2: Int, function: () -> Unit) {
-        }
 }
